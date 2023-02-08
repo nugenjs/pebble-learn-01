65a1ed4da6dd6bcf1a12841f7474c86dc70cf306
nugenjs <nugenjs@gmail.com>
2023-02-08 02:25:08 -0600

    fonts

--- src/c/watchface02.c
+++ src/c/watchface02.c
@@ -3,7 +3,7 @@
 static Window *s_window;
 static TextLayer *s_text_layer;
 static TextLayer *s_time_layer;
-static GFont *s_time_font;
+static GFont s_time_font;
 
 static void update_time() {
   // Get a tm structure
@@ -26,6 +26,7 @@ static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
+
   // Create the TextLayer with specific bounds
   s_time_layer = text_layer_create(
       GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
@@ -33,14 +34,22 @@ static void main_window_load(Window *window) {
   // Improve the layout to be more like a watchface
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlue);
+
+  // Custom fonts load
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
+  // Apply to TextLayer
+  text_layer_set_font(s_time_layer, s_time_font);
+
   // text_layer_set_text(s_time_layer, "04:20");
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  // text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
   // // Add it as a child layer to the Window's root layer
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 }
 static void main_window_unload(Window *window) {
+  fonts_unload_custom_font(s_time_font);
+
   // Destroy TextLayer
   text_layer_destroy(s_time_layer);
 }
